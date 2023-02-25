bee779d7bf428fe51a1fbe850fb4d88982d13378
ivanatomic43 <ivana.tomic96@gmail.com>
2023-02-25 17:26:47 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './css/style.css'
+import Homepage from './pages/Homepage'
+import FlowersPage from './pages/FlowersPage'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Routes } from 'react-router'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/flowers' element={<FlowersPage />} />
+      </Routes>
+    </Router>
+   </>
+  )
 }
 
-export default App;
+export default App
