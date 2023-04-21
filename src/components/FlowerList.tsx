3bc399663cf1326dd523d369e1d9ac9a9043a625
ivanatomic43 <ivana.tomic96@gmail.com>
2023-04-21 19:37:24 +0200

    Personalized Flower Card

--- src/components/FlowerList.tsx
+++ src/components/FlowerList.tsx
@@ -1,13 +1,18 @@
 import FlowerListItem from "./FlowerListItem"
+import PersonalizedFlowerListItem from "./PersonalizedFlowerListItem"
 
 function FlowerList() : JSX.Element {
   return (
     <div className='container'>
-       <div className='flower-container'>
-        <FlowerListItem />
-        <FlowerListItem />
-        <FlowerListItem />
-        <FlowerListItem />
+       <div className='flower-container bg-gray-200'>
+        <PersonalizedFlowerListItem />
+        <PersonalizedFlowerListItem />
+        <PersonalizedFlowerListItem />
+        <PersonalizedFlowerListItem />
+        <PersonalizedFlowerListItem />
+        <PersonalizedFlowerListItem />
+        <PersonalizedFlowerListItem />
+        <PersonalizedFlowerListItem />
       </div>
     </div>
   )
--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -30,7 +30,7 @@ const useUserData = () => {
 
       getUserData();
     }
-  }, []);
+  });
 
   return { userData }
 }
